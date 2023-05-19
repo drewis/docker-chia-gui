8f7b30792f6fd5275e18c8965710bdfdb21da987
Drew Sutherland <git@drewis.fun>
2023-05-19 17:40:07 -0600

    updates for latest

--- gui/root/etc/cont-init.d/10-chiainit.sh
+++ gui/root/etc/cont-init.d/10-chiainit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv sh
 export HOME=/chia
 
-s6-setuidgid chia chia init
-s6-setuidgid chia chia keys add -f ${HOME}/.chia/mnemonic.txt
+s6-setuidgid chia chia init --fix-ssl-permissions
+s6-setuidgid chia chia keys add -f ${HOME}/.chia/mnemonic.txt -l ""
 
